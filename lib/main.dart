7f3f7ab9f009614111d0baf8bc2152b4440a398f
Marc_The_Shark <marc.piper@blindside.pro>
2020-04-09 01:07:27 +0200

    Successful token retrieval

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,48 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
 import 'package:http_server/http_server.dart';
-import 'package:sountify/services/spotify_service.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+import 'package:sountify/services/spotify_service.dart';
 import 'package:sountify/screens/loading_screen.dart';
 
 void main() async {
   await DotEnv().load('.env');
   await HttpServer.bind(InternetAddress.loopbackIPv4, 8888).then((server) {
-    print('Server running at: ${server.
-    address.address}');
+    print('Server running at: ${server.address.address}');
     server.transform(HttpBodyHandler()).listen((HttpRequestBody body) async {
       print(body.request.uri.toString());
       body.request.response.headers.set("Content-Type", "application/json");
-      body.request.response.headers.add("Access-Control-Allow-Methods", "POST, OPTIONS, GET");
+      body.request.response.headers
+          .add("Access-Control-Allow-Methods", "POST, OPTIONS, GET");
       body.request.response.headers.add("Access-Control-Allow-Origin", "*");
       body.request.response.headers.add('Access-Control-Allow-Headers', '*');
       body.request.response.headers.add('X-Frame-Options', '*');
-      if(body.request.uri.toString().contains('callback')){
-        body.request.response.write('pups');
-      }
-      else body.request.response.write('pinki ponko');
+      if (body.request.uri.toString().contains('callback')) {
+        http.Response tokenResponse =
+            await http.post('https://accounts.spotify.com/api/token', headers: {
+          'Authorization':
+              'Basic ${base64.encode(utf8.encode('${SpotifyService.clientID}:${SpotifyService.secretKey}'))}'
+        }, body: {
+          'grant_type': 'authorization_code',
+          'code': body.request.uri.queryParameters['code'],
+          'redirect_uri': SpotifyService.redirectURI
+        });
+
+      } else
+        body.request.response.write('pinki ponko');
       body.request.response.close();
     });
   });
-
-//  var server = await HttpServer.bind(InternetAddress.loopbackIPv4,8888).then((server) {
-//    server.transform(HttpBodyHandler()).listen((HttpRequestBody body) async {
-//      print("request inc");
-//      body.request.response.headers.set("Content-Type", "text/html; charset=utf-8");
-//      body.request.response.headers.add("Access-Control-Allow-Methods", "POST, OPTIONS, GET");
-//      body.request.response.headers.add("Access-Control-Allow-Origin", "*");
-//      body.request.response.headers.add('Access-Control-Allow-Headers', '*');
-//      body.request.response.headers.add('X-Frame-Options', '*');
-//      body.request.response.write(spotifyResponse.body);
-//      body.request.response.close();
-//    });
-////    request.response.headers.contentType = ContentType('text' , 'html');
-////    request.response.write(spotifResponse.body);
-//  });
 
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
--- lib/services/spotify_service.dart
+++ lib/services/spotify_service.dart
@@ -2,12 +2,12 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:sountify/services/common.dart';
 
 class SpotifyService extends Service {
-  static const String url = 'https://api.spotify.com/v1/';
-  final String clientID = DotEnv().env['SPOTIFY_CLIENT_ID'];
-  final String secretKey = DotEnv().env['SPOTIFY_CLIENT_SECRET'];
+  static const String apiURL = 'https://api.spotify.com/v1/';
+  static final String clientID = DotEnv().env['SPOTIFY_CLIENT_ID'];
+  static final String secretKey = DotEnv().env['SPOTIFY_CLIENT_SECRET'];
+  static const redirectURI = 'http://localhost:8888/callback/';
 
   String get authorizeURL {
-    String redirectURI = 'http://localhost:8888/callback/';
     return 'https://accounts.spotify.com/authorize?client_id=$clientID&response_type=code&'
         'redirect_uri=$redirectURI&scope=streaming%20user-modify-playback-state%20playlist-read-collaborative%20'
         'playlist-read-private%20user-read-currently-playing%20'
